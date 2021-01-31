24b37fc1a56b76f8627c333569ed38ecf9a32db9
Konrad <chrzanekk@gmail.com>
2021-01-31 18:39:35 +0100

    db connection in progress

--- src/main/java/pl/konradchrzanowski/scaffolding/log/scaffolding/reviews/Review.java
+++ src/main/java/pl/konradchrzanowski/scaffolding/log/scaffolding/reviews/Review.java
@@ -2,5 +2,7 @@ package pl.konradchrzanowski.scaffolding.log.scaffolding.reviews;
 
 public class Review {
     private Long ReviewId;
+//    przemyśleć jak rozwiązać sposób zaznaczania przeglądów dekadowych. czy zrobić to w formie przypomnienia co
+//    dziesięć dni?
 
 }
